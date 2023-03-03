f6ab343a5348b1fc71a2ecf6db1bb31f9b4128a7
Moritz Fink <moritz.fink@accenture.com>
2023-03-03 12:33:07 +0100

    Remove useless granted authority

--- src/main/kotlin/io/github/finkmoritz/springgrouprolepermissions/auth/MyUserPrincipal.kt
+++ src/main/kotlin/io/github/finkmoritz/springgrouprolepermissions/auth/MyUserPrincipal.kt
@@ -2,16 +2,13 @@ package io.github.finkmoritz.springgrouprolepermissions.auth
 
 import io.github.finkmoritz.springgrouprolepermissions.entity.user.User
 import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class MyUserPrincipal(
     val user: User
 ) : UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        return mutableListOf(
-            SimpleGrantedAuthority("ROLE_USER") //TODO dummy authority needed to pass authentication
-        )
+        return mutableListOf()
     }
 
     override fun getPassword(): String {
