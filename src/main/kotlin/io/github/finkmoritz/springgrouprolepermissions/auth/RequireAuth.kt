5a6c40e5d4b50cfecab1a258cd71113d0a54d6b6
Moritz Fink <moritz.fink@accenture.com>
2023-03-08 11:51:47 +0100

    Add doc for annotation

--- src/main/kotlin/io/github/finkmoritz/springgrouprolepermissions/auth/RequireAuth.kt
+++ src/main/kotlin/io/github/finkmoritz/springgrouprolepermissions/auth/RequireAuth.kt
@@ -1,7 +1,20 @@
 package io.github.finkmoritz.springgrouprolepermissions.auth
 
+/**
+ * Annotation for Controller methods which require authentication.
+ *
+ * @param requireGroupRoles Array of required roles for the requested Group
+ * If you provide an Array of requireGroupRoles, then the Aspect checks them against the
+ * roles in the principal and denies the request if not found. Since the roles to be checked are
+ * defined per Group, the method call MUST CONTAIN the groupId (Long) !
+ *
+ * @param requireGroupPermissions Array of required permissions for the requested Group
+ * If you provide an Array of requireGroupPermissions, then the Aspect checks them against the
+ * permissions in the principal and denies the request if not found. Since the permissions to be checked are
+ * defined per Group, the method call MUST CONTAIN the groupId (Long) !
+ */
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 annotation class RequireAuth(
     val requireGroupRoles: Array<String> = [],
     val requireGroupPermissions: Array<String> = [],
